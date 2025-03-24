715b947eefb4478aa4d7dc5b7ef886af22928475
Joseph Rimsky <joseph.rimsky@geckorobotics.com>
2025-03-24 15:44:14 -0400

    ..

--- .github/scripts/python.sh
+++ .github/scripts/python.sh
@@ -99,8 +99,8 @@ function package()
   done
 
   for whl in $GITHUB_WORKSPACE/wheelhouse/*.whl; do
-    new_filename=$(echo $whl | sed "s#\.none-win_amd64\.#.#g")
-    new_filename=$(echo $new_filename | sed "s#\.win_amd64\.#.#g") # For 37 and 38
+    new_filename=$(echo $whl | sed "s#/.none-win_amd64/.#.#g")
+    new_filename=$(echo $new_filename | sed "s#/.win_amd64/.#.#g") # For 37 and 38
     new_filename=$(echo $new_filename | sed "s#-none-#-#g")
     mv $whl $new_filename
   done
